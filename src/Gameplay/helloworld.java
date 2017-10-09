0538a919faa147c8538c5c6c98b56335f1be9bcd
shilsa <30322616+shilsa@users.noreply.github.com>
2017-10-09 21:16:49 +0700

    Update helloworld.java

--- src/Gameplay/helloworld.java
+++ src/Gameplay/helloworld.java
@@ -8,8 +8,6 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello again");
-		// TODO Auto-generated method stub
-
 	}
 
 }
