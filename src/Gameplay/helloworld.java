74d983f6b82541e9157c4f21a0c0a229578f273f
shilsa <dragoonhome@gmail.com>
2017-10-09 17:33:25 +0700

    Added a folder

---
+++ src/Gameplay/helloworld.java
@@ -0,0 +1,14 @@
+package Gameplay;
+
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
