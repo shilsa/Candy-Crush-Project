06e321501cf7c08eaaa944c08c4f1e16fa7ee78b
shilsa <dragoonhome@gmail.com>
2017-10-09 17:26:09 +0700

    Added a folder

---
+++ testing game/src/Lit/game.java
@@ -0,0 +1,14 @@
+package Lit;
+
+public class game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
