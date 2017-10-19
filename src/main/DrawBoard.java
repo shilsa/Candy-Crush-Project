435213c11424288921ff085d0479eb419029c680
shilsa <dragoonhome@gmail.com>
2017-10-19 11:52:05 +0700

    update

--- src/main/DrawBoard.java
+++ src/main/DrawBoard.java
@@ -1,19 +1,24 @@
 package main;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class DrawBoard {
+public class DrawBoard{
 	public static final int WIDTH = 800, HEIGHT = 600;
 	
 	public void drawBoard(){
 		JFrame frame = new JFrame("Candy Crush");
+		JLabel label = new JLabel("");
+		frame.setVisible(true);
 		frame.setSize(WIDTH,HEIGHT);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
+		label.setText("Hello World");
+		label.setToolTipText("Its basic stuff");
+		frame.add(label);
 	}
 
 }
--- src/main/mainCC.java
+++ src/main/mainCC.java
@@ -1,10 +1,7 @@
 package main;
 
 public class mainCC {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		DrawBoard draw = new DrawBoard();
 		draw.drawBoard();
