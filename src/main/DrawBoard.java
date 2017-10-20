b056fa44ebba97979896831c9039adac51f6a969
shilsa <dragoonhome@gmail.com>
2017-10-20 13:09:46 +0700

    Few changes

--- src/main/DrawBoard.java
+++ src/main/DrawBoard.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class DrawBoard{
+public class DrawBoard extends JFrame{
 	public static final int WIDTH = 800, HEIGHT = 600;
 	
 	public void drawBoard(){
@@ -19,7 +19,17 @@ public class DrawBoard{
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		label.setIcon(image);
+		label.setVisible(true);
 		frame.add(label);
+	}
+	public void DrawTest(){
+			JLabel Label = new JLabel();
+			add(Label);
+			setSize(WIDTH, HEIGHT);
+			setLocation(600,300);
+			setVisible(true);
+			ImageIcon icon = new ImageIcon("BG.jpg");
+			Label.setIcon(icon);
 	}
 
 }
