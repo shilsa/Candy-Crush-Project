851bedfaf4fb5be38147c957b7093f220cda2a0b
shilsa <dragoonhome@gmail.com>
2017-10-23 17:41:37 +0700

    Score(1)

--- src/main/DrawBoard.java
+++ src/main/DrawBoard.java
@@ -5,18 +5,25 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public class DrawBoard extends JPanel{
 	private ImageIcon BG;
 	private ImageIcon tittle;
+	JLabel Score = new JLabel();
 	protected void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paintComponent(g); 
+		// draw BG
 		BG = new ImageIcon("BG1.jpg");
 		BG.paintIcon(this, g, 0, 0);
+		// draw tittle
 		tittle = new ImageIcon("TittleImage.png");
 		tittle.paintIcon(this, g, 145, 5);
+		// draw Score
+		g.setColor(Color.WHITE);
+		g.drawString("Score: ", 10, 100);
 	}
 }
