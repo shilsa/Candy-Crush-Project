9a5b165a5282a8421af5b599c4b44dc2f6e09190
An <annguyen16498@gmail.com>
2017-12-11 14:07:01 +0700

    ...

---
+++ src/UI/Data.java
@@ -0,0 +1,30 @@
+
+package game;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Data {
+    public static Data data = new Data();
+    
+    /**
+     *
+     * @return
+     */
+    public static Data getInstance(){
+        return data;
+    }
+
+    public List<Player> Player_list = new ArrayList<>();
+
+    public List<Player> getPlayer_list(){
+        return Player_list;
+    }
+    
+    public void addPlayer(Player objPlayer){
+        Player_list.add(objPlayer);
+    }
+    
+}
